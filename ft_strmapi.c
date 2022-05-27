27e9ae9adbd87e7780ee79a769c31dbdb520b32d
samiyazbr <94432143+samiyazbr@users.noreply.github.com>
2022-05-27 09:51:06 +0400

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szubair <szubair@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 09:48:30 by szubair           #+#    #+#             */
+/*   Updated: 2022/05/24 09:01:57 by szubair          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, (char)s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
