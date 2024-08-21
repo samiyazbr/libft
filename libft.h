d7b5aa7250ad8cfaf412a59c3678fa0db4f845b3
samiyazbr <samiyazubair1414@gmail.com>
2024-08-21 19:34:29 +0400

    linked list added

--- libft.h
+++ libft.h
@@ -22,6 +22,12 @@
 # include <fcntl.h>
 # define SIZE_T_MAX 18446744073709551615UL
 
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
+
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
@@ -58,4 +64,13 @@ void	ft_putstr_fd(char *s, int fd);
 int		ft_toupper(int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strdup(char *src);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int	ft_lstsize(t_list *lst);
 #endif
